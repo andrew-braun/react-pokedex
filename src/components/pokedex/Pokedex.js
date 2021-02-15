eb6796f04fa9ace6f4fdb0e26c2d698b655e4bb5
Andrew Braun <andrew@andrewbraun.dev>
2021-02-15 14:49:25 +0400

    feat: Finish API call structure and card processing

--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -1,27 +1,49 @@
 import { useEffect, useState } from "react"
-import PokemonCard from "../pokemoncard/PokemonCard"
+import PokemonCard from "../../components/pokemoncard/PokemonCard"
 import styles from "./pokedex.module.css"
 
 function Pokedex() {
 	const [pokemonData, setPokemonData] = useState()
-	const [pokemonCards, setPokemonCards] = useState()
+	const [pokemonStats, setPokemonStats] = useState([])
+	const [pokemonCards, setPokemonCards] = useState([])
 
 	useEffect(() => {
-		async function fetchPokemon(call) {
+		// Make a call to get a single Pokemon's data
+		async function fetchSinglePokemon(call) {
 			try {
 				const response = await fetch(call)
 				const data = await response.json()
-				await setPokemonData(data)
-				console.log(data.results)
-				const cards = await data.results.map((pokemon) => (
-					<PokemonCard pokemonData={pokemon} key={`${pokemon.name}-key`} />
-				))
-				await setPokemonCards(cards)
+				console.log(data)
+				// Store Pokemon data in an array of objects
+				await setPokemonStats((previousPokemonStats) => [
+					previousPokemonStats,
+					data,
+				])
+
+				// Store Pokemon in card component form for rendering
+				const card = <PokemonCard pokemonData={data} key={`${data.id}-key`} />
+				await setPokemonCards((previousPokemonCards) => [
+					previousPokemonCards,
+					card,
+				])
 			} catch (error) {
 				console.log(error)
 			}
 		}
-		fetchPokemon("https://pokeapi.co/api/v2/pokemon?limit=151")
+
+		// Fetch a list of all pokemon
+		async function fetchPokemonList(call) {
+			try {
+				const response = await fetch(call)
+				const data = await response.json()
+				await setPokemonData(data)
+
+				await data.results.map((pokemon) => fetchSinglePokemon(pokemon.url))
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		fetchPokemonList("https://pokeapi.co/api/v2/pokemon?limit=3")
 	}, [])
 
 	return <div className={styles.pokedex}>{pokemonCards}</div>
