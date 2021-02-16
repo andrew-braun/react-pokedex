ccf21b533764c8b02bf8b0c6ed6c4cd70a221496
Andrew Braun <andrew@andrewbraun.dev>
2021-02-16 17:09:59 +0400

    content: Add description to sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ function Layout({ children }) {
 	return (
 		<div className={styles.layout}>
 			<header className={styles.header}>
-				<div className={styles.siteTitle}>React Pokedex</div>
+				<div className={styles.siteTitle}>React Pokédex</div>
 			</header>
 			<main className={styles.main}>{children}</main>
 			<footer className={styles.footer}>Made with ❤️ by Andrew Braun</footer>
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -4,7 +4,11 @@ function Sidebar() {
 	return (
 		<aside className={styles.sidebar}>
 			<header className={styles.sidebarHeader}>
-				<div className={styles.sidebarTitle}></div>
+				<div className={styles.sidebarTitle}>Build Your Pokédeck!</div>
+				<p className={styles.sidebarDescription}>
+					Select up to 20 Pokémon, then test them against a randomly selected
+					set!
+				</p>
 			</header>
 			<div className={styles.sidebarItems}></div>
 		</aside>
