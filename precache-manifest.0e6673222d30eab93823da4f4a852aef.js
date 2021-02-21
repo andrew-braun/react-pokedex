ed8bec4531a542a7700887d73aca36b4149f9232
Andrew Braun <andrew@andrewbraun.dev>
2021-02-21 16:50:14 +0400

    Updates

---
+++ precache-manifest.0e6673222d30eab93823da4f4a852aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2759144cf64e9b4d5507",
+    "url": "/react-pokedex/static/css/main.8de4d1d7.chunk.css"
+  },
+  {
+    "revision": "2759144cf64e9b4d5507",
+    "url": "/react-pokedex/static/js/main.7e311376.chunk.js"
+  },
+  {
+    "revision": "c4612808a03fc2a15021",
+    "url": "/react-pokedex/static/js/runtime~main.4e4f56be.js"
+  },
+  {
+    "revision": "867df2045e3aeea805f9",
+    "url": "/react-pokedex/static/js/2.32236d53.chunk.js"
+  },
+  {
+    "revision": "7d73936799d589e68d79",
+    "url": "/react-pokedex/static/js/3.a270207b.chunk.js"
+  },
+  {
+    "revision": "732e041a3e95652f42a3a7afdf3362c2",
+    "url": "/react-pokedex/index.html"
+  }
+];
